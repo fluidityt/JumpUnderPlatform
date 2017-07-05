6bea11a15de5409f568c5b754451d9c894f86ed6
fluidityt <hackbook17@justins-macbook-pro.local>
2017-07-04 20:44:54 -0400

    Initial Commit

---
+++ JumpUnderPlatform/JumpUnderPlatform.swift
@@ -0,0 +1,14 @@
+//
+//  JumpUnderPlatform.swift
+
+
+  
+  
+class JumpUnderPlatform: SKSpriteNode {
+  
+  init(color: SKColor, size: CGSize) {
+    super.init(texture: nil, color: color, size: size)
+  }
+  
+  required init?(coder aDecoder: NSCoder) { fatalError() }
+}
