4ecd4d31ff9859a6397d95910cbd575b9426c5d8
fluidityt <hackbook17@justins-macbook-pro.local>
2017-07-07 01:59:20 -0400

    final

--- JumpUnderPlatform/BitMasks.swift
+++ JumpUnderPlatform/BitMasks.swift
@@ -6,7 +6,4 @@ import SpriteKit
 struct BitMasks {
   static let playerCategory = UInt32(2)
   static let jupCategory    = UInt32(4)
-  
-  static let testCollision1 = UInt32(8)
-  static let testCollision2 = UInt32(16)
 }
--- JumpUnderPlatform/GameScene.swift
+++ JumpUnderPlatform/GameScene.swift
@@ -7,30 +7,10 @@ import GameplayKit
 // MARK: - Props:
 class GameScene: SKScene, SKPhysicsContactDelegate {
 
-  let jupName = "jup"
+  // Because I hate crashes related to spelling errors.
+  let names = (jup: "jup", resetLabel: "resetLabel")
   
   let player = Player.makePlayer()
-
-
-  var contactedPlatform: JumpUnderPlatform?
-  
-}
-
-// MARK: - Funky:
-extension GameScene {
-  
-  // Testing purposes:
-
-  func getJup() -> JumpUnderPlatform {
-     return childNode(withName: jupName)! as! JumpUnderPlatform
-  }
-  
-  // Scene setup:
-  override func didMove(to view: SKView) {
-    physicsWorld.contactDelegate = self
-    physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-    addChild(player)
-  }
 }
 
 
@@ -38,11 +18,11 @@ extension GameScene {
 extension GameScene {
   
   private func findJup(contact: SKPhysicsContact) -> JumpUnderPlatform? {
-    guard let nodeA = contact.bodyA.node, let nodeB = contact.bodyB.node else { fatalError() }
+    guard let nodeA = contact.bodyA.node, let nodeB = contact.bodyB.node else { fatalError("how did this happne!!??") }
     
-    if      nodeA.name == jupName { return (nodeA as! JumpUnderPlatform) }
-    else if nodeB.name == jupName { return (nodeB as! JumpUnderPlatform) }
-    else                          { return nil }
+    if      nodeA.name == names.jup { return (nodeA as! JumpUnderPlatform) }
+    else if nodeB.name == names.jup { return (nodeB as! JumpUnderPlatform) }
+    else                            { return nil }
   }
   
   // Player is 2, platform is 4:
@@ -51,13 +31,17 @@ extension GameScene {
     // Check if jumping; if not, then just land on platform normally.
     guard player.initialDY > 0 else { return }
 
-    // platform.physicsBody!.collisionBitMask = BitMasks.jupCategory
+    // Gives us the ability to pass through the platform!
     player.physicsBody!.collisionBitMask = BitMasks.playerCategory
+    
+    // Will push the player through the platform (instead of bouncing off) on first hit
+    if player.platformToPassThrough == nil { player.pb.velocity.dy = player.initialDY }
     player.platformToPassThrough = platform
   }
   
   func _didBegin(_ contact: SKPhysicsContact) {
   
+    // Crappy way to do bit-math:
     let contactedSum = contact.bodyA.categoryBitMask + contact.bodyB.categoryBitMask
     
     switch contactedSum {
@@ -65,6 +49,9 @@ extension GameScene {
     case BitMasks.jupCategory + BitMasks.playerCategory:
       guard let platform = findJup(contact: contact) else { fatalError("must be platform!") }
       doContactPlayer_X_Jup(platform: platform)
+      
+    // Put your other contact cases here...
+    // case BitMasks.xx + BitMasks.yy:
       
     default: ()
     }
@@ -74,13 +61,26 @@ extension GameScene {
 // MARK: - Game loop:
 extension GameScene {
   
-  // Touch handling:
+  // Scene setup:
+  override func didMove(to view: SKView) {
+    physicsWorld.contactDelegate = self
+    physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    addChild(player)
+  }
+  
+  // Touch handling: (convert to touchesBegan for iOS):
   override func mouseDown(with event: NSEvent) {
+    // Make player jump:
     player.pb.applyImpulse(CGVector(dx: 0, dy: 50))
+    
+    // Reset player on label click:
+    if nodes(at: event.location(in: self)).first?.name == names.resetLabel {
+      player.position.y = frame.minY + player.size.width/2 + CGFloat(1)
+    }
   }
   
   override func update(_ currentTime: TimeInterval) {
-    player._update() // Updates initialDY
+    player._update()
   }
   
   func didBegin(_ contact: SKPhysicsContact) {
